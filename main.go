4833cc915e527c98359223468bed47dd3272d55f
daiba <badaibou5@gmail.com>
2023-07-11 22:14:41 +0000

    groupie commit

---
+++ main.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type PageData struct {
+	ID           int
+	Image        string
+	Name         string
+	Members      []string
+	CreationDate int
+	FirstAlbum   string
+	Locations    []string
+	ConcertDates []string
+	Relations    map[string][]string
+}
+
+type ArtistFullData struct {
+	ID           int                 `json:"id"`
+	Image        string              `json:"image"`
+	Name         string              `json:"name"`
+	Members      []string            `json:"members"`
+	CreationDate int                 `json:"creationDate"`
+	FirstAlbum   string              `json:"firstAlbum"`
+	Locations    []string            `json:"locations"`
+	ConcertDates []string            `json:"concertDates"`
+	Relations    map[string][]string `json:"relations"`
+}
+
+type MyArtist struct {
+	ID           int                 `json:"id"`
+	Image        string              `json:"image"`
+	Name         string              `json:"name"`
+	Members      []string            `json:"members"`
+	CreationDate int                 `json:"creationDate"`
+	FirstAlbum   string              `json:"firstAlbum"`
+	Locations    string              `json:"locations"`
+	ConcertDates string              `json:"concertDates"`
+	Relations    map[string][]string `json:"relations"`
+}
+
+type MyLocation struct {
+	ID        int      `json:"id"`
+	Locations []string `json:"locations"`
+	Dates     string   `json:"dates"`
+}
+type LocationData struct {
+	Index []MyLocation `json:"index"`
+}
+
+type MyConcertDate struct {
+	ID    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+type ConcertDateData struct {
+	Index []MyConcertDate `json:"index"`
+}
+
+type MyRelationDate struct {
+	ID            int                 `json:"id"`
+	DatesLocation map[string][]string `json:"datesLocations"`
+}
+type RelationData struct {
+	Index []MyRelationDate `json:"index"`
+}
+
+var Artists []MyArtist
+var ArtistData []ArtistFullData
+var LocationsData LocationData
+var ConcertDatesData ConcertDateData
+var RelationsData RelationData
+
+func GetArtistsData() error {
+
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		return errors.New("Error by get")
+	}
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.New("Error by ReadAll")
+	}
+	json.Unmarshal(bytes, &Artists)
+	return nil
+}
+
+func GetLocations() error {
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/locations")
+	if err != nil {
+		return errors.New("Error by get")
+	}
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.New("Error by ReadAll")
+	}
+	json.Unmarshal(bytes, &LocationsData)
+	return nil
+}
+
+func GetDates() error {
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/dates")
+	if err != nil {
+		return errors.New("Error by get")
+	}
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.New("Error by ReadAll")
+	}
+	json.Unmarshal(bytes, &ConcertDatesData)
+	return nil
+}
+
+func GetRelations() error {
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/relation")
+	if err != nil {
+		return errors.New("Error by get")
+	}
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.New("Error by ReadAll")
+	}
+	json.Unmarshal(bytes, &RelationsData)
+	return nil
+}
+
+func GetData() {
+	GetArtistsData()
+	GetLocations()
+	GetDates()
+	GetRelations()
+	var template ArtistFullData
+	// var locate MyLocation
+	for i := range Artists {
+		template.ID = i + 1
+		template.Image = Artists[i].Image
+		template.Name = Artists[i].Name
+		template.Members = Artists[i].Members
+		template.CreationDate = Artists[i].CreationDate
+		template.FirstAlbum = Artists[i].FirstAlbum
+		template.Locations = LocationsData.Index[i].Locations
+		template.ConcertDates = ConcertDatesData.Index[i].Dates
+		template.Relations = RelationsData.Index[i].DatesLocation
+
+		ArtistData = append(ArtistData, template)
+	}
+	return
+}
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	template, _ := template.ParseFiles("./templates/index.html")
+
+	template.Execute(w, ArtistData)
+}
+func artistHandler(w http.ResponseWriter, r *http.Request) {
+	template, _ := template.ParseFiles("./templates/ArtistPage.html")
+
+	queryParams := r.URL.Query() // Obtient les paramètres de la requête dans un map
+
+	id := queryParams.Get("id")
+	data := PageData{}
+
+	for _, v := range ArtistData {
+		a, _ := strconv.Atoi(id)
+		if v.ID == a {
+			data.Name = v.Name
+			data.Members = v.Members
+			data.Image = v.Image
+			data.CreationDate = v.CreationDate
+			data.ConcertDates = v.ConcertDates
+			data.FirstAlbum = v.FirstAlbum
+			data.Locations = v.Locations
+			data.Relations = v.Relations
+		}
+	}
+
+	template.Execute(w, data)
+}
+func main() {
+	GetData()
+	http.HandleFunc("/", MainHandler)
+	http.HandleFunc("/artist/", artistHandler)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	log.Println("http://localhost:9000")
+
+	http.ListenAndServe(":9000", nil)
+}
